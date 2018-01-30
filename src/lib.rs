6230f1e73d70c99721172c88b8324ac69a8b6650
Simon Walker <s.r.walker101@googlemail.com>
2018-01-30 20:29:46 +0000

    Add settings object

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ extern crate serde;
 extern crate serde_derive;
 extern crate serde_json;
 
+use std::default::Default;
 use failure::Error;
 
 pub mod responses;
@@ -21,6 +22,38 @@ pub const URL: &'static str = mockito::SERVER_URL;
 pub const URL: &'static str = "https://api.zoopla.co.uk";
 
 pub type Result<T> = std::result::Result<T, Error>;
+
+pub struct ZooplaQuerySettings {
+    pub radius: u32,
+    pub postcode: String,
+    pub listing_status: String,
+    pub include_sold: bool,
+    pub include_rented: bool,
+    pub minimum_price: u32,
+    pub maximum_price: u32,
+    pub minimum_beds: u32,
+    pub property_type: String,
+    pub new_homes: bool,
+    pub page_size: u32,
+}
+
+impl Default for ZooplaQuerySettings {
+    fn default() -> Self {
+        ZooplaQuerySettings {
+            radius: 10,
+            postcode: "CV22DX".to_string(),
+            listing_status: "sale".to_string(),
+            include_sold: false,
+            include_rented: false,
+            minimum_price: 250000,
+            maximum_price: 500000,
+            minimum_beds: 4,
+            property_type: "houses".to_string(),
+            new_homes: false,
+            page_size: 100,
+        }
+    }
+}
 
 pub struct Zoopla {
     pub api_key: String,
@@ -36,23 +69,22 @@ impl Zoopla {
         })
     }
 
-    pub fn properties(&mut self) -> Result<HousesResponse> {
+    pub fn properties(&mut self, settings: ZooplaQuerySettings) -> Result<HousesResponse> {
         let mut resp = self.client
             .get(&self.listings_url())
             .query(&[
                 ("api_key", &self.api_key),
-                ("radius", &"10".to_string()),
-                ("postcode", &"CV22DX".to_string()),
-                ("listing_status", &"sale".to_string()),
-                ("include_sold", &"0".to_string()),
-                ("include_rented", &"0".to_string()),
-                ("minimum_price", &"250000".to_string()),
-                ("maximum_price", &"500000".to_string()),
-                ("minimum_beds", &"4".to_string()),
-                ("property_type", &"houses".to_string()),
-                ("new_homes", &"false".to_string()),
-                // TODO: increase page size
-                ("page_size", &"10".to_string()),
+                ("radius", &settings.radius.to_string()),
+                ("postcode", &settings.postcode),
+                ("listing_status", &settings.listing_status),
+                ("include_sold", &settings.include_sold.to_string()),
+                ("include_rented", &settings.include_rented.to_string()),
+                ("minimum_price", &settings.minimum_price.to_string()),
+                ("maximum_price", &settings.maximum_price.to_string()),
+                ("minimum_beds", &settings.minimum_beds.to_string()),
+                ("property_type", &settings.property_type),
+                ("new_homes", &settings.new_homes.to_string()),
+                ("page_size", &settings.page_size.to_string()),
             ])
             .send()?
             .error_for_status()?;
@@ -88,7 +120,7 @@ mod tests {
             dotenv::dotenv().ok();
             let zoopla_key = env::var("ZOOPLA_KEY").unwrap();
             let mut api = Zoopla::new_session(&zoopla_key).unwrap();
-            let properties = api.properties().unwrap();
+            let properties = api.properties(ZooplaQuerySettings::default()).unwrap();
             let listings = properties.listing;
             assert_eq!(listings.len(), 10);
         });
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate dotenv;
 extern crate zooplaapi;
 
 use std::env;
-use zooplaapi::{Result, Zoopla};
+use zooplaapi::{Result, Zoopla, ZooplaQuerySettings};
 
 fn main() {
     dotenv::dotenv().ok();
@@ -15,7 +15,7 @@ fn main() {
 fn run() -> Result<()> {
     let zoopla_key = env::var("ZOOPLA_KEY")?;
     let mut api = Zoopla::new_session(&zoopla_key)?;
-    let properties = api.properties()?;
+    let properties = api.properties(ZooplaQuerySettings::default())?;
     for property in properties.listing {
         println!("Property: {:?}", property);
     }
