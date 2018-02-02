3cb25aa4e938b24eb82b5d6d8b4f397e08de39af
Simon Walker <s.r.walker101@googlemail.com>
2018-02-02 23:42:09 +0000

    Add insertables for all models

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -9,14 +9,20 @@ pub fn foo() {
     use db::schema::houses_categories::dsl::*;
 
     let connection = connection::establish_connection().expect("connecting to database");
-    let results: Vec<(String, i32)> = houses
-        .filter(price.eq(500000))
-        .inner_join(houses_categories)
-        .select((property_type, price))
-        .load(&connection)
-        .expect("error loading houses");
+    connection
+        .transaction::<_, ::diesel::result::Error, _>(|| {
+            let results: Vec<(String, i32)> = houses
+                .filter(price.eq(500000))
+                .inner_join(houses_categories)
+                .select((property_type, price))
+                .load(&connection)
+                .expect("error loading houses");
 
-    for value in results {
-        println!("{:?}", value);
-    }
+            for value in results {
+                println!("{:?}", value);
+            }
+
+            Ok(())
+        })
+        .expect("failed to run transaction");
 }
--- src/db/models.rs
+++ src/db/models.rs
@@ -1,17 +1,49 @@
-// use super::schema::*;
+use super::schema::*;
 
-#[derive(Queryable, Debug)]
-pub struct House {
-    pub id: i32,
+#[derive(Insertable)]
+#[table_name = "houses"]
+pub struct NewHouse<'a> {
     pub price: i32,
-    pub first_published_date: String,
-    pub last_published_date: String,
+    pub first_published_date: &'a str,
+    pub last_published_date: &'a str,
 }
 
-#[derive(Queryable, Debug)]
-pub struct Category {
-    pub id: i32,
-    pub property_type: String,
-    pub category: String,
-    pub price_modifier: Option<String>,
+#[derive(Insertable)]
+#[table_name = "houses_agents"]
+pub struct NewAgent<'a> {
+    pub name: &'a str,
+    pub phone: &'a str,
+}
+
+#[derive(Insertable)]
+#[table_name = "houses_categories"]
+pub struct NewCategory<'a> {
+    pub property_type: &'a str,
+    pub category: &'a str,
+    pub price_modifier: Option<&'a str>,
+}
+
+#[derive(Insertable)]
+#[table_name = "houses_locations"]
+pub struct NewLocation<'a> {
+    pub longitude: f32,
+    pub latitude: f32,
+    pub street_name: &'a str,
+    pub displayable_address: &'a str,
+}
+
+#[derive(Insertable)]
+#[table_name = "houses_properties"]
+pub struct NewProperty<'a> {
+    pub description: &'a str,
+    pub num_bathrooms: i32,
+    pub num_bedrooms: i32,
+    pub num_floors: i32,
+}
+
+#[derive(Insertable)]
+#[table_name = "houses_urls"]
+pub struct NewUrls<'a> {
+    pub details: &'a str,
+    pub property_report: &'a str,
 }
