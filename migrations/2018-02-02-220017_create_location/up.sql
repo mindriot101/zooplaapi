e2d2f68e32a87e67eb3b25aa9258c555271fc317
Simon Walker <s.r.walker101@googlemail.com>
2018-02-02 22:10:47 +0000

    Add locations columns

--- migrations/2018-02-02-220017_create_location/up.sql
+++ migrations/2018-02-02-220017_create_location/up.sql
@@ -3,5 +3,8 @@
 CREATE TABLE locations (
     id SERIAL PRIMARY KEY,
     longitude REAL NOT NULL,
-    latitude REAL NOT NULL
+    latitude REAL NOT NULL,
+    street_name VARCHAR(255) NOT NULL,
+    displayable_address VARCHAR(255) NOT NULL,
+    house_id INTEGER REFERENCES houses(id)
 )
